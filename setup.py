60a09aba5191335ec66b5ef13aceaaef790be472
Arthur Kantor <kantor@cs.wisc.edu>
2023-06-05 16:21:52 +0200

    allow py 3.9 and 3.10 in setup.py

--- setup.py
+++ setup.py
@@ -75,6 +75,8 @@ def setup_package():
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
         ],
     )
 
